f8b24572dce08af870ab793024129c40fee7bba7
Jess Bracht <jbrachtie@gmail.com>
2024-03-27 15:24:18 -0700

    Add Sentry

--- excalidraw-app/sentry.ts
+++ excalidraw-app/sentry.ts
@@ -2,25 +2,20 @@ import * as Sentry from "@sentry/browser";
 import * as SentryIntegrations from "@sentry/integrations";
 
 const SentryEnvHostnameMap: { [key: string]: string } = {
-  "draw-next.sparkwise.co": "production",
-  "vercel.app": "staging",
+  "draw-prod.sparkwise.co": "production",
+  "draw-next.sparkwise.co": "staging",
 };
 
-const SENTRY_DISABLED = import.meta.env.VITE_APP_DISABLE_SENTRY === "true";
-
 // Disable Sentry locally or inside the Docker to avoid noise/respect privacy
-const onlineEnv =
-  !SENTRY_DISABLED &&
-  Object.keys(SentryEnvHostnameMap).find(
-    (item) => window.location.hostname.indexOf(item) >= 0,
-  );
+const onlineEnv = Object.keys(SentryEnvHostnameMap).find(
+  (item) => window.location.hostname.indexOf(item) >= 0,
+);
 
 Sentry.init({
   dsn: onlineEnv
-    ? "https://7bfc596a5bf945eda6b660d3015a5460@sentry.io/5179260"
+    ? "https://37fe4be0271ac8f2d1de035c5d32d3c7@o1112051.ingest.us.sentry.io/4506979770171392"
     : undefined,
   environment: onlineEnv ? SentryEnvHostnameMap[onlineEnv] : undefined,
-  release: import.meta.env.VITE_APP_GIT_SHA,
   ignoreErrors: [
     "undefined is not an object (evaluating 'window.__pad.performLoop')", // Only happens on Safari, but spams our servers. Doesn't break anything
   ],
@@ -29,10 +24,4 @@ Sentry.init({
       levels: ["error"],
     }),
   ],
-  beforeSend(event) {
-    if (event.request?.url) {
-      event.request.url = event.request.url.replace(/#.*$/, "");
-    }
-    return event;
-  },
 });
